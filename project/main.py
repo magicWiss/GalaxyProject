99f924aea275f54c71a75b40af1b22461c00e4b8
magicWiss <mat.wissel@stud.uniroma3.it>
2022-07-07 10:59:34 +0200

    Commit 07/07/22
    Secondo commit
    installazione di kernel notebook

--- project/main.py
+++ project/main.py
@@ -1,2 +1,20 @@
+import os
+
 if __name__=="__main__":
-    print("hello world, i am starting a new adventure in ML")
+    
+    path = "../images"
+    # change the working directory to the path where the images are located
+    os.chdir(path)
+
+    # this list holds all the image filename
+    galaxys = []
+
+    # creates a ScandirIterator aliased as files
+    with os.scandir(path) as files:
+    # loops through each file in the directory
+        for file in files:
+            if file.name.endswith('.jpg'):
+            # adds only the image files to the flowers list
+                galaxys.append(file.name)
+
+    print(galaxys)
