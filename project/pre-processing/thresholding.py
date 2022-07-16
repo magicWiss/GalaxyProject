a3d910cbda9ca6a7c173d9b39c7999c6d5913f2f
magicWiss <mat.wissel@stud.uniroma3.it>
2022-07-16 17:07:29 +0200

    Commit 16/07/2022- Pre-processing Images (thresolding)
    Creazione della classe di threshold delle immagini.
    
    Il threshold permette di eliminare eventuali pixel inutili settandoli a 0.
    Se un pixel non supera una soglia data, allora viene settato a 0.
    
    La nuova img viene calcolata come:
    
    img_old*mat_threshOld

---
+++ project/pre-processing/thresholding.py
@@ -0,0 +1,53 @@
+
+import cv2 as cv
+import numpy as np
+from matplotlib import  cm, pyplot as plt
+
+
+class ThresholdImg:
+    def __init__(self):
+        
+       pass
+        
+
+
+    
+    def threshImageOtsu(self,image):
+        
+        th3,ret3 =cv.threshold(img,0,1,cv.THRESH_BINARY+cv.THRESH_OTSU)
+        imgBlur=np.multiply(ret3,img)
+        plt.imshow(imgBlur,cmap="gray")
+        plt.show()
+        return imgBlur
+
+    def threshImageBinary(self,image):
+        
+        threshold=np.max(img)*0.1
+        
+        ret1,th1 = cv.threshold(img,threshold,1,cv.THRESH_BINARY)
+        maxrth=np.max(ret1)
+        
+        imgBlur=np.multiply(th1,img)
+       
+        plt.imshow(imgBlur,cmap='gray')
+        plt.show()
+        return imgBlur
+        
+
+if __name__=='__main__':
+    imgPath='../images/training/100335.jpg'
+    img = cv.imread(imgPath)
+    thres=ThresholdImg()
+    img=thres.threshImageBinary(img)
+    
+
+
+
+
+
+
+
+
+
+
+ 
