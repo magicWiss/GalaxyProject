f75dc91e3510c18fa90446b6eb0cc1f49aabf518
magicWiss <mat.wissel@stud.uniroma3.it>
2022-09-22 14:30:47 +0200

    fix knn model
    Il metodo adesso fa: cross validation in modo corretto

--- project/models/knn.py
+++ project/models/knn.py
@@ -1,4 +1,5 @@
 
+from statistics import mean
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report, confusion_matrix
@@ -16,54 +17,38 @@ class KNN:
 
         pass
 
-    def predict(self, X_Train,Y_Train, X_Test, Y_Test):
-        print("\nAddestramento di un modello KNN")
-        knn = KNeighborsClassifier(n_neighbors=1)
-       
-        knn.fit(X_Train,Y_Train)
-        
-
-        #predizione del modello
-        pred = knn.predict(X_Test)
-
-       
-        print(confusion_matrix(Y_Test,pred))
-        print(classification_report(Y_Test,pred))
-        
+    def predict(self, X_Train,Y_Train, X_Test, Y_Test):        
         #Scelta del valore di k: Per ogni valore di k chiameremo classificatore KNN e quindi sceglieremo il valore di k che ha il minor tasso di errore
         error_rate = []
-        error4k={}
         max_k=20
-
-        
-
+        best_meanError=np.inf
         for i in range(1,max_k+1):
     
-            knn = KNeighborsClassifier(n_neighbors=i)
+            knn = KNeighborsClassifier(n_neighbors=i, algorithm='kd_tree')
             knn.fit(X_Train,np.array(Y_Train))
 
             #CROSS VALIDATION
-            CV = CrossValidation(5)
-            pred_i = CV.k_folds.kFoldCV(X_Train, np.array(Y_Train), knn)
-          
+            from sklearn.model_selection import cross_val_score
+            #train model with cv of 5 
+            cv_scores = cross_val_score(knn, X_Train, np.ravel(Y_Train), cv=5)
+            #print each cv score (accuracy) and average them
             
-            #confronto
-            Y_Test=np.array(Y_Test)
-            rate=np.mean(pred_i!=Y_Test)
-            error_rate.append(rate)
-            error4k[i]=np.mean(rate)
+            print("Errore per ogni k-blocci:",cv_scores)
+            mean_error=np.mean(cv_scores)
+            print("Errore medio con k=",i,"->",mean_error)
+            error_rate.insert(i,mean_error)
+            if (mean_error<=best_meanError):
+                best_meanError=mean_error 
+                    
 
-        #print("Cross Validation Scores: ", pred_i.scores)
-        #print("Average CV Score: ", pred_i.scores.mean())
-        #print("Number of CV Scores used in Average: ", pred_i.len(scores))  
 
         #Tracciamo un grafico a linee del tasso di errore
         print("\nTracciamo un grafico a linee del tasso di errore")
         plt.figure(figsize=(10,6))
-        plt.plot(range(1,max_k+1),error_rate,color='blue', linestyle='dashed', marker='o', markerfacecolor='red', markersize=10)
+        plt.plot(range(1,max_k+1,1),error_rate,color='blue', linestyle='dashed', marker='o', markerfacecolor='red', markersize=10)
         plt.title('Error Rate vs. K Value')
         plt.xlabel('K')
-        plt.ylabel('Error Rate')
+        plt.ylabel('Errore medio K-fold')
         plt.show()
 
 
@@ -71,19 +56,21 @@ class KNN:
         #iniziale preso come esempio
         #altrimenti dovremo fare una selezione del minimo dell' Error rate e memorizzare in una variabile il suo K associato così da metterla qui e controllare le differenze
         
-        mini=min(error4k,key=error4k.get)
-        knn = KNeighborsClassifier(n_neighbors=mini,algorithm='kd_tree')
+        minimumK=error_rate.index(min(error_rate))  
+        
+        knn = KNeighborsClassifier(n_neighbors=minimumK+1,algorithm='kd_tree')
 
-        knn.fit(X_Train,Y_Train)
+        knn.fit(X_Train,np.ravel(Y_Train))
         
         
         #addestriamo con il test set
         pred = knn.predict(X_Test)
 
 
-        print('WITH K=',mini)
+        print('Accuracy con k=',minimumK+1)
+        print(accuracy_score(Y_Test, pred))
         print('\n')
-        print(confusion_matrix(Y_Test,pred))
+        print("Matrice di confusione\n",confusion_matrix(Y_Test,pred))
         print('\n')
         print(classification_report(Y_Test,pred))
 
@@ -95,19 +82,7 @@ class KNN:
 
 
 
-        #qui si implementa il metodo per l'addesteamento del modello
-        #si inseriscono qui le operazioni di:
-        #PCA e scelta del numero di componenti ottimali
-        #cross-validation per la scelta del numero di k ottimi per il modello
-        #stampa del risultato dell'addestramento
-        #model=KNeighborsClassifier()
-        #model.fit(X_Train,Y_Train)
-        #predicted_test=model.predict(X_Test)
-        #acc=accuracy_score(Y_Test,predicted_test)
-
-        #print("KNN Accuricy:",acc)
-        #print(confusion_matrix(Y_Test, predicted_test))
-        #print(classification_report(Y_Test, predicted_test))
+        
 
 
         
